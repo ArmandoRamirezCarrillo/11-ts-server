e5cc715888cab30574d269d152e7e7900a613a4e
Jose Armando Ramirez Carrillo <ing.armandoramirez@outlook.com>
2025-01-16 18:07:27 -0600

    Rest server typescript

--- models/server.ts
+++ models/server.ts
@@ -1,12 +1,47 @@
 import express, {Application} from 'express';
+import userRoutes from '../routes/usuario';
+import cors from 'cors';
+import db from '../db/connection';
 
 class Server{
     private app: Application;
     private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    }
 
     constructor(){
         this.app = express();
         this.port = process.env.PORT || '8000';
+        //DataBase
+        this.dbConnection();
+        //Middlewares
+        this.middlewares();
+        //Definir mis rutas
+        this.routes();
+    }
+
+    //TODO conectar base de datos
+    async dbConnection(){
+        try {
+            await db.authenticate();
+            console.log('DataBase Online');
+        } catch (error) {
+            throw new Error(`${error}`);
+        }
+    }
+
+    middlewares(){
+        //CORS
+        this.app.use(cors());
+        //Lectura del body
+        this.app.use(express.json());
+        //Carpeta publica
+        this.app.use(express.static('public'));
+    }
+
+    routes(){
+        this.app.use(this.apiPaths.usuarios, userRoutes);
     }
 
     listen(){
