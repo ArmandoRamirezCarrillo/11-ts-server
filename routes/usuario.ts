c737e6067b96377c0b11b2c86111fee156b49048
Jose Armando Ramirez Carrillo <ing.armandoramirez@outlook.com>
2025-01-16 18:15:43 -0600

    Cambios en las rutas

--- routes/usuario.ts
+++ routes/usuario.ts
@@ -1,12 +1,18 @@
-import { Router } from 'express';
+import { RequestHandler, Router } from 'express';
 import { deleteUsuario, getUsuario, getUsuarios, postUsuario, putUsuario } from '../controllers/usuarios';
 
 const router = Router();
 
-router.get('/', getUsuarios);
-router.get('/:id', getUsuario);
-router.post('/', postUsuario);
-router.put('/:id', putUsuario);
-router.delete('/:id', deleteUsuario);
+// router.get('/', getUsuarios);
+// router.get('/:id', getUsuario);
+// router.post('/', postUsuario);
+// router.put('/:id', putUsuario);
+// router.delete('/:id', deleteUsuario);
+
+router.get('/', getUsuarios as RequestHandler);
+router.get('/:id', getUsuario as RequestHandler);
+router.post('/', postUsuario as RequestHandler);
+router.put('/:id', putUsuario as RequestHandler);
+router.delete('/:id', deleteUsuario as RequestHandler);
 
 export default router;
